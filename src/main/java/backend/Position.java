29e4621c02d96973162a6aef10b713d507320c52
saumya796 <64645381+saumya796@users.noreply.github.com>
2020-09-21 15:03:59 +0530

    c3:Updated game

--- src/main/java/backend/Position.java
+++ src/main/java/backend/Position.java
@@ -2,70 +2,31 @@ package backend;
 
 public class Position {
 
-    private double x1;
-    private double x2;
-    private int x;
-    private int y;
-    private double y1;
-    private double y2;
+    private double x;
+    private double y;
 
     public Position(double newX, double newY) {
-
-    }
-
-    public int getX() {
-        return x;
-    }
-    public void setX(int newX) {
         this.x = newX;
-    }
-    public double getX1() {
-        return x1;
-    }
-    public void setX1(double newX1) {
-        this.x1 = newX1;
-    }
-    public double getX2() {
-        return x2;
-    }
-    public void setX2(double newX2) {
-        this.x2 = newX2;
-    }
-    public int getY() {
-        return y;
-    }
-    public void setY(int newY) {
         this.y = newY;
     }
-    public double getY1() {
-        return y1;
-    }
-    public void setY1(double newY1) {
-        this.y1 = newY1;
-    }
-    public double getY2() {
-        return y2;
-    }
-    public void setY2(int newY2) {
-        this.y2 = newY2;
-    }
-    public Position(int x, int y, double x1, double x2, double y1, double y2) {
-        this.x = x;
-        double x1Meters;
-        this.y = y;
-        this.x1 = x1;
-        this.x2 = x2;
-        this.y1 = y1;
-        this.y2 = y2;
-    }
 
-    public double getx() {
+
+    public double getX() {
         return x;
+    }
 
+    public void setX(double x) {
+        this.x = x;
     }
-    public double gety2() {
-        return y2;
+
+    public double getY() {
+        return y;
     }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     public String printPosition() {
         return "[" + (Math.round(x * 100.0) / 100.0) + "," + (Math.round(y * 100.0) / 100.0) + "]";
     }
--- src/test/java/backend/PlayerTest.java
+++ src/test/java/backend/PlayerTest.java
@@ -1,7 +1,46 @@
 package backend;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    @Test
+    public void testCalcPosition1() {
+
+        Player player = new Player("Amal");
+        Position initPosition = new Position(3, 4);
+        Position endPosition = new Position(0, 0);
+        Position newPlayerPosition = player.calcPosition(initPosition, endPosition, 2);
+    }
+
+    @Test
+    public void testCalcDistance2() {
+        Player player = new Player("Amal");
+        Position initPosition = new Position(1, 1);
+        Position endPosition = new Position(2, 2);
+        double testDistance = player.calcDistance(initPosition, endPosition);
+        assertEquals(Math.sqrt(2), testDistance);
+    }
+
+    @Test
+    public void testCalcPosition6() {
+
+        Player player = new Player("Amal");
+        Position initPosition = new Position(0, 0);
+        Position endPosition = new Position(0, 5);
+        Position newPlayerPosition = player.calcPosition(initPosition, endPosition, 2);
+        assertEquals(0, newPlayerPosition.getX());
+        assertEquals(2, newPlayerPosition.getY());
+    }
+
+    @Test
+    public void testCalcDistance1() {
+        Player player = new Player("Kamal");
+        Position initPosition = new Position(1, 2);
+        Position endPosition = new Position(10, 2);
+        double testDistance = player.calcDistance(initPosition, endPosition);
+        assertEquals(9, testDistance);
+    }
 }
